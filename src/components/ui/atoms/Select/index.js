9f2576588c48978450e16cf4173ec9d39d3e131c
Kevin Fery <kevin.fery@outlook.com>
2024-05-18 13:37:41 +0200

    add select

---
+++ src/components/ui/atoms/Select/index.js
@@ -0,0 +1,35 @@
+import './input.scss'
+import cn from '../../../../utils/classnames'
+
+const Select = ({
+    label,
+    id,
+    placeholder,
+    required = false,
+    className
+}) => {
+    return <div className={cn(
+        'select',
+        {
+            filter: type === 'search',
+            text: type === 'text',
+        },
+        className,
+    )}>
+        {label && <label
+            for={id}
+            className={'select__label'}
+        >
+            {label}
+            {required && <span className='select__required'>*</span>}
+        </label>}
+        <select
+            className={'select__target'}
+            type={type}
+            id={id}
+            placeholder={placeholder}
+        />
+    </div>
+}
+
+export default Select
