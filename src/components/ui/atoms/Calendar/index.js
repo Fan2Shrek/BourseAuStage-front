4ef08266dd2b3ddff4a617db6d2571f2d6762415
Kevin Fery <kevin.fery@outlook.com>
2024-05-20 21:07:57 +0200

    remove calendar

--- src/components/ui/atoms/Calendar/index.js
+++ src/components/ui/atoms/Calendar/index.js
@@ -1,15 +1,8 @@
 import './calendar.scss';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 const Calendar = () => {
-    return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DatePicker />
-        </LocalizationProvider>
-      );
-};
 
+  return
+};
 
 export default Calendar;
