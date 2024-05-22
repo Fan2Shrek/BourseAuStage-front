9031a9ea6876e388e40689a5938780da8f995b8d
FeryKevin <kevin.fery@outlook.com>
2024-05-22 11:59:56 +0200

    add calendar

--- src/components/ui/atoms/Calendar/index.js
+++ src/components/ui/atoms/Calendar/index.js
@@ -1,8 +1,16 @@
 import './calendar.scss';
+import * as React from 'react';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 const Calendar = () => {
 
-  return
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker />
+    </LocalizationProvider>
+  );
 };
 
 export default Calendar;
