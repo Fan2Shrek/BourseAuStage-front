5499b58091f6bd7556cb58b98e939a1ceed145a1
FeryKevin <kevin.fery@outlook.com>
2024-05-24 18:14:27 +0200

    add front

--- src/components/ui/atoms/Calendar/index.js
+++ src/components/ui/atoms/Calendar/index.js
@@ -3,12 +3,17 @@ import * as React from 'react';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import TextField from '@mui/material/TextField';
 
 const Calendar = () => {
-
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DatePicker />
+      <DatePicker
+        inputFormat="DD/MM/YYYY"
+        renderInput={(params) => (
+          <TextField {...params} variant="outlined" className="custom-date-picker" />
+        )}
+      />
     </LocalizationProvider>
   );
 };
