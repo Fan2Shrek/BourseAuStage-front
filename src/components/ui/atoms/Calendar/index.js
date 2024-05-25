c529ba1fbe3d22274df1a4e56cf30cf4c78987bf
FeryKevin <kevin.fery@outlook.com>
2024-05-25 18:11:50 +0200

    add french calendar

--- src/components/ui/atoms/Calendar/index.js
+++ src/components/ui/atoms/Calendar/index.js
@@ -4,25 +4,30 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import TextField from '@mui/material/TextField';
+import 'dayjs/locale/fr';
 
 const Calendar = ({
   label = null,
   id,
   required = false,
 }) => {
-  return <>
-    {label && <label
-      htmlFor={id}
-    >
-      {label}
-      {required && <span className='input__required'>*</span>}
-    </label>}
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DatePicker
-        id={id}
-      />
-    </LocalizationProvider>
-  </>
+  return (
+    <>
+      {label && (
+        <label htmlFor={id}>
+          {label}
+          {required && <span className='input__required'>*</span>}
+        </label>
+      )}
+      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="fr">
+        <DatePicker
+          id={id}
+          renderInput={(params) => <TextField {...params} />}
+          inputFormat="DD/MM/YYYY"
+        />
+      </LocalizationProvider>
+    </>
+  );
 };
 
 export default Calendar;
